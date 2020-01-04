6ea35daaccc7ce72964ade4a628c92c591c75f07
misterluck <zl876170926@sina.com>
2020-01-04 13:18:00 +0800

    CommitInit

--- spring-boot-antdcommon/src/main/java/org/example/common/constant/CommonConstant.java
+++
@@ -1,8 +0,0 @@
-package org.example.common.constant;
-
-public class CommonConstant {
-
-    /**字典翻译文本后缀*/
-    public static final String DICT_TEXT_SUFFIX = "_dictText";
-
-}
--- spring-boot-antdvue/src/main/java/org/example/antdvue/constant/AuthConstant.java
+++
@@ -1,7 +0,0 @@
-package org.example.antdvue.constant;
-
-public class AuthConstant {
-
-    public static final String ACCESS_TOKEN = "Access-Token";
-
-}
