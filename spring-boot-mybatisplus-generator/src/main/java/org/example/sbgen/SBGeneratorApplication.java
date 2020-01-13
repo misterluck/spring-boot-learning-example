9fdeb037d03576d592581ba92b6f4d58581c2423
misterluck <zl876170926@sina.com>
2020-01-13 11:36:06 +0800

    动态生成代码CommitInit

---
+++ spring-boot-mybatisplus-generator/src/main/java/org/example/sbgen/SBGeneratorApplication.java
@@ -0,0 +1,4 @@
+package org.example.sbgen;
+
+public class SBGeneratorApplication {
+}
---
+++ spring-boot-mybatisplus-generator/src/main/java/org/example/sbgen/generator/MysqlGenerator.java
@@ -0,0 +1,133 @@
+package org.example.sbgen.generator;
+
+import com.baomidou.mybatisplus.core.toolkit.StringPool;
+import com.baomidou.mybatisplus.generator.AutoGenerator;
+import com.baomidou.mybatisplus.generator.InjectionConfig;
+import com.baomidou.mybatisplus.generator.config.*;
+import com.baomidou.mybatisplus.generator.config.po.TableFill;
+import com.baomidou.mybatisplus.generator.config.po.TableInfo;
+import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
+import com.baomidou.mybatisplus.generator.engine.FreemarkerTemplateEngine;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * <p>
+ * mybatis plus mysql 代码生成器
+ * </p>
+ *
+ */
+public class MysqlGenerator {
+
+    /**
+     * RUN THIS
+     */
+    public static void main(String[] args) {
+        // 代码生成器
+        AutoGenerator mpg = new AutoGenerator();
+
+        // 全局配置
+        GlobalConfig gc = new GlobalConfig();
+        // String projectPath = System.getProperty("user.dir");
+        String projectPath = (String.valueOf(Thread.currentThread().getContextClassLoader().getResource(""))).replaceAll("file:/", "").replaceAll("%20", " ").replace("target/classes/", "").trim();
+        gc.setOutputDir(projectPath + "/src/main/java");
+        gc.setAuthor("zhaolei");
+        gc.setOpen(true);
+        //service 命名方式
+        gc.setServiceName("I%sService");
+        //service impl 命名方式
+        gc.setServiceImplName("I%sServiceImpl");
+        // 自定义文件命名，注意 %s 会自动填充表实体属性！
+        gc.setMapperName("I%sMapper");
+        gc.setXmlName("I%sMapper");
+        gc.setFileOverride(true);
+        gc.setActiveRecord(true);
+        // XML 二级缓存
+        gc.setEnableCache(false);
+        // XML ResultMap
+        gc.setBaseResultMap(true);
+        // XML columList
+        gc.setBaseColumnList(false);
+        mpg.setGlobalConfig(gc);
+
+        // 数据源配置
+        DataSourceConfig dsc = new DataSourceConfig();
+        dsc.setUrl("jdbc:mysql://10.21.171.61:3306/antdvue?useUnicode=true&useSSL=false&characterEncoding=utf8&serverTimezone=UTC");
+        dsc.setDriverName("com.mysql.jdbc.Driver");
+        dsc.setUsername("puaiuc");
+        dsc.setPassword("Ailk@2019");
+        mpg.setDataSource(dsc);
+
+        // 包配置
+        PackageConfig pc = new PackageConfig();
+        //pc.setModuleName(scanner("模块名"));
+        pc.setParent("org.example.sbgen");
+        pc.setEntity("entity");
+        pc.setService("service");
+        pc.setServiceImpl("service.impl");
+        mpg.setPackageInfo(pc);
+
+        // 自定义需要填充的字段
+        List<TableFill> tableFillList = new ArrayList<>();
+        //如 每张表都有一个创建时间、修改时间
+        //而且这基本上就是通用的了，新增时，创建时间和修改时间同时修改
+        //修改时，修改时间会修改，
+        //虽然像Mysql数据库有自动更新几只，但像ORACLE的数据库就没有了，
+        //使用公共字段填充功能，就可以实现，自动按场景更新了。
+        //如下是配置
+        //TableFill createField = new TableFill("gmt_create", FieldFill.INSERT);
+        //TableFill modifiedField = new TableFill("gmt_modified", FieldFill.INSERT_UPDATE);
+        //tableFillList.add(createField);
+        //tableFillList.add(modifiedField);
+
+        // 自定义配置
+        InjectionConfig cfg = new InjectionConfig() {
+            @Override
+            public void initMap() {
+                // to do nothing
+            }
+        };
+        List<FileOutConfig> focList = new ArrayList<>();
+
+        focList.add(new FileOutConfig("/templates/mapper.xml.ftl") {
+            @Override
+            public String outputFile(TableInfo tableInfo) {
+                // 自定义输入文件名称
+                return projectPath + "/src/main/resources/mapper/"
+                        + "/" + tableInfo.getEntityName() + "Mapper" + StringPool.DOT_XML;
+            }
+        });
+
+        cfg.setFileOutConfigList(focList);
+        mpg.setCfg(cfg);
+
+        // 配置模板
+        TemplateConfig templateConfig = new TemplateConfig();
+
+        // 配置自定义输出模板
+        //指定自定义模板路径，注意不要带上.ftl/.vm, 会根据使用的模板引擎自动识别
+        templateConfig.setXml(null);
+        templateConfig.setController("/generator/controller.java");
+
+        mpg.setTemplate(templateConfig);
+
+        // 策略配置
+        StrategyConfig strategy = new StrategyConfig();
+        strategy.setNaming(NamingStrategy.underline_to_camel);
+        strategy.setColumnNaming(NamingStrategy.underline_to_camel);
+        strategy.setEntityLombokModel(true);
+        strategy.setRestControllerStyle(true);
+        // 设置逻辑删除键
+        strategy.setLogicDeleteFieldName("deleted");
+        strategy.setInclude("auth_info_admin");
+        //strategy.setSuperEntityColumns("id");
+        //驼峰转连字符
+        strategy.setControllerMappingHyphenStyle(true);
+        mpg.setStrategy(strategy);
+        // 选择 freemarker 引擎需要指定如下加，注意 pom 依赖必须有！
+        mpg.setTemplateEngine(new FreemarkerTemplateEngine());
+        mpg.execute();
+    }
+
+}
