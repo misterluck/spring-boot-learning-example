a3107d79e26334ee56848dfa071cb2543f4edd9a
zhaolei <zhaolei3@asiainfo-sec.com>
2022-09-27 19:02:11 +0800

    netty实现http与https协议

--- spring-boot-netty/src/main/java/org/example/sbnetty/netty/initializer/HttpsServerInitializer.java
+++ spring-boot-netty/src/main/java/org/example/sbnetty/netty/initializer/HttpsServerInitializer.java
@@ -6,10 +6,7 @@ import io.netty.channel.ChannelPipeline;
 import io.netty.channel.socket.nio.NioSocketChannel;
 import io.netty.handler.codec.http.*;
 import io.netty.handler.ssl.OptionalSslHandler;
-import io.netty.handler.ssl.SslContext;
-import io.netty.handler.ssl.SslContextBuilder;
 import io.netty.handler.ssl.SslHandler;
-import io.netty.handler.ssl.util.SelfSignedCertificate;
 import org.example.sbnetty.netty.handler.HttpServerHandler;
 import org.example.sbnetty.netty.handler.NettyProxyServerHandler;
 import org.example.sbnetty.netty.util.SslUtil;
@@ -17,8 +14,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.net.ssl.SSLEngine;
-import javax.net.ssl.SSLException;
-import java.security.cert.CertificateException;
 
 /**
  * @author zhaol
@@ -40,14 +35,15 @@ public class HttpsServerInitializer extends ChannelInitializer<NioSocketChannel>
             // 同时支持http与https
             pipeline.addLast(new OptionalSslHandler(sslCtx));
         }*/
-
+        // 仅支持https
         pipeline.addLast(SslUtil.getServerSslCtx().newHandler(ch.alloc()));
+        // 同时支持http与https
         // pipeline.addLast(new OptionalSslHandler(SslUtil.getServerSslCtx()));
         pipeline.addLast(new HttpServerCodec());
         pipeline.addLast(new HttpObjectAggregator(1024 * 1024 * 1024));
         pipeline.addLast(new HttpServerExpectContinueHandler());
-        // pipeline.addLast(httpServerHandler);
-        pipeline.addLast(new NettyProxyServerHandler(SslUtil.getClientSslCtx()));
+        pipeline.addLast(httpServerHandler);
+        // pipeline.addLast(new NettyProxyServerHandler(SslUtil.getClientSslCtx()));
 
     }
 
